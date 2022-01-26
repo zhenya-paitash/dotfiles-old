cdaad566ba6b5dfe149484b89bfe3e8e825642fa
zhenya-paitash <naitavv93@gmail.com>
2022-01-26 16:14:55 +0300

    create '.config' folder & powershell profile settings

---
+++ .config/powershell/user_profile.ps1
@@ -0,0 +1,19 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+
+# Posh Theme 'https://ohmyposh.dev/docs/themes' 
+Set-PoshPrompt Paradox
+# Set-PoshPrompt M365Princess
+# Set-PoshPrompt bubbles
+
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+# MY Alias
+Set-Alias e explorer
