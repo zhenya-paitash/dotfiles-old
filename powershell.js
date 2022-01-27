ffe9b263b50e4fc796d0c8b9a6504b5eaffa4a0d
zhenya-paitash <naitavv93@gmail.com>
2022-01-27 18:27:31 +0300

    add more steps

--- powershell.js
+++ powershell.js
@@ -2,12 +2,12 @@ const fs = require("fs")
 const powershell = "./json/powershell.json"
 const powershellScheme = "./json/powershellScheme.json"
 
-function readAsync(file) {
+function readFile(file) {
   const data = fs.readFileSync(file).toString()
   const json = JSON.parse(data)
-  const steps =  Object.keys(json.steps)
+  const steps = Object.keys(json.steps)
 
-  return steps.forEach(i => console.log(json.steps[i]))
+  return steps.forEach(i => console.log(i, ':', json.steps[i]))
 }
 
-readAsync(powershell)
+readFile(powershell)
