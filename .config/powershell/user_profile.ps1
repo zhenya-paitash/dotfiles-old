66973115e55c057fd11c0306433ebc9ffa3caf6c
zhenya-paitash <naitavv93@gmail.com>
2022-01-28 22:00:29 +0300

    upd user_profile.ps1 powershell config

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,19 +1,41 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
 # Prompt
 Import-Module posh-git
 Import-Module oh-my-posh
 
-# Posh Theme 'https://ohmyposh.dev/docs/themes' 
+# Posh Scheme 
 Set-PoshPrompt Paradox
-# Set-PoshPrompt M365Princess
-# Set-PoshPrompt bubbles
 
+# Load prompt config
+$OMP_CONFIG = Join-Path $PSScriptRoot ".\zhenya.omp.json"
+oh-my-posh --init --shell pwsh --config $OMP_CONFIG | Invoke-Expression
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# FZF
+Import-Module PSFzf
+Set-PsFzfOption -SPReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 # Alias
-Set-Alias vim nvim
+Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
-# MY Alias
 Set-Alias e explorer
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
